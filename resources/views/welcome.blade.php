1daf3b93180fbd6dae0308b8b4c3a82c4a791196
Nico Deblauwe <nico@bluepundit.eu>
2024-11-13 12:53:01 +0100

    REFACTOR :hammer: Move welcome to controller (and add articles)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 <x-site-layout title="super BCN blog">
 
-    Hello class
+    @foreach($articles as $article)
+        <div class="mt-4">
+            <h2 class="font-bold text-lg">{{$article->title}}</h2>
+            <div>
+                {{ \Carbon\Carbon::parse($article->published_at)->toDateString() }}
+                |
+                {{$article->author_name}}
+            </div>
+            <p class="text-sm">{{$article->content}}</p>
+        </div>
+    @endforeach
 
+
+    hello class
 </x-site-layout>
