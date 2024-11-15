22b9830e7f0480860f6677c30a479cb9c9d264eb
Nico Deblauwe <nico@bluepundit.eu>
2024-11-15 19:10:20 +0100

    DESIGN :art: Change logo to same of blog

--- resources/views/layouts/site/menu.blade.php
+++ resources/views/layouts/site/menu.blade.php
@@ -2,8 +2,8 @@
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
-                <div class="shrink-0">
-                    <img class="h-8 w-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                <div class="shrink-0 text-blue-300">
+                    <a href="/">SuperBCN</a>
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
