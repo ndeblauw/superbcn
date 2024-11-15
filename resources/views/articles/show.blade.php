6199e92b0921362337266933ce9a5ccf90b704ef
Nico Deblauwe <nico@bluepundit.eu>
2024-11-15 19:04:46 +0100

    FEATURE :sparkles: Add detailed view for one article

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,13 @@
+<x-site-layout title="{{$article->title}}">
+
+    <div class="max-w-4xl mx-auto">
+        <p class="text-xl">
+            {{$article->content}}
+        </p>
+
+    </div>
+
+
+
+</x-site-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Http\Controllers\WelcomeController::class);
 
+Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
