f414acdbd988f15100118dbf3fca0bae4e27e256
Nico Deblauwe <nico@bluepundit.eu>
2024-11-18 23:08:08 +0100

    DESIGN :art: Minimal styling the article CRUD

--- resources/views/user/articles/edit.blade.php
+++ resources/views/user/articles/edit.blade.php
@@ -1,25 +1,24 @@
-<x-site-layout>
+<x-site-layout title="Edit article">
 
-    <form action="{{route('user.articles.update', $article)}}" method="post">
+    <form action="{{route('user.articles.update', $article)}}" method="post" class="w-2/3 border border-gray-300 p-4">
         @method('PUT')
         @csrf
 
-        <label for="title">Title</label><br/>
-        <input name="title" value="{{old('title', $article->title)}}">
-        @error('title')<div class="text-red-500">{{$message}}</div> @enderror
+        <label for="title" class="block text-xs font-semibold uppercase">Title</label>
+        <input name="title" value="{{old('title', $article->title)}}" class="w-2/3 p-1 rounded-lg border border-gray-200 @error('title') border-red-500 @enderror">
+        @error('title')<div class="text-red-500 text-xs">{{$message}}</div> @enderror
 
         <br/><br/>
 
-        <label for="content">Content</label><br/>
-        <textarea name="content">{{old('content', $article->content)}}</textarea>
-        @error('content')<div class="text-red-500">{{$message}}</div> @enderror
+        <label for="content" class="block text-xs font-semibold uppercase">Content</label>
+        <textarea name="content" class="w-full p-1 rounded-lg border border-gray-200 @error('content') border-red-500 @enderror" rows="5">{{old('content', $article->content)}}</textarea>
+        @error('content')<div class="text-red-500 text-xs">{{$message}}</div> @enderror
 
         <br/><br/>
 
-        <div>
-            <a href="{{route('user.articles.index')}}">Undo</a>
-            <button type="submit">Save changes</button>
-
+        <div class="w-full flex justify-end gap-x-8">
+            <a href="{{route('user.articles.index')}}" class="text-xs text-gray-700 bg-gray-300 hover:bg-gray-200 px-4 py-2 rounded uppercase">Undo</a>
+            <button type="submit" class="text-xs text-green-700 bg-green-300 hover:bg-green-200 px-4 py-2 rounded uppercase">Save changes</button>
         </div>
     </form>
 
--- resources/views/user/articles/index.blade.php
+++ resources/views/user/articles/index.blade.php
@@ -1,6 +1,10 @@
-<x-site-layout>
+<x-site-layout title="Overview of my articles">
 
-    <a href="{{route('user.articles.create')}}">create article</a>
+    <div class="flex justify-end mb-4">
+        <a class="text-xs text-green-700 bg-green-300 px-1 py-.5 rounded uppercase" href="{{route('user.articles.create')}}">create article</a>
+
+    </div>
+
 
     @if(session()->has('success'))
         <div class="bg-green-100 text-green-500 p-2">
@@ -8,14 +12,29 @@
         </div>
     @endif
 
-    <ul class="list-disc pl-4">
-        @foreach($articles as $article)
-            <li>
-                {{$article->title}}
-                <a href="{{route('user.articles.edit', $article)}}">edit</a>
-                <form action="{{route('user.articles.destroy', $article)}}" method="post">@method('delete')@csrf<button type="submit">delete</button></form>
-            </li>
-        @endforeach
-    </ul>
+    <table class="table-auto w-full border border-gray-300">
+        <thead>
+            <tr class="bg-gray-300 ">
+                <th>Title</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($articles as $article)
+                <tr class="hover:bg-gray-200 border-b border-gray-200">
+                    <td><a href="{{route('user.articles.show', $article)}}">{{$article->title}}</a></td>
+                    <td class="flex gap-x-4 justify-center items-center">
+                        <a href="{{route('user.articles.edit', $article)}}" class="text-xs text-blue-700 bg-blue-300 px-1 py-.5 rounded uppercase">edit</a>
+                        <form action="{{route('user.articles.destroy', $article)}}" method="post">
+                            @method('delete')
+                            @csrf
+                            <button type="submit" class="text-xs text-red-700 bg-red-300 px-1 py-.5 rounded uppercase">delete</button>
+                        </form>
+                    </td>
+                <tr/>
+            @endforeach
+        </tbody>
+    </table>
 
 </x-site-layout>
