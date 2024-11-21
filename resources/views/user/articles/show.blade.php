83747598665f3a003ad89558ab09bb4fd64b8279
Nico Deblauwe <nico@bluepundit.eu>
2024-11-21 12:51:15 +0100

    CONTENT :pencil:: Add information on author and categories to show view

--- resources/views/user/articles/show.blade.php
+++ resources/views/user/articles/show.blade.php
@@ -1,5 +1,9 @@
 <x-site-layout title="{{$article->title}}">
 
+    <div>by {{$article->author->name}}</div>
+
+    <div>@foreach($article->categories as $category) {{$category->title}} @endforeach</div>
+
     <img src="{{$article->media->first()->getUrl('preview')}}">
 
     {{$article->content}}
