1772294bedc74ddd22498af5d9f53df3fd1fdbb2
Nico Deblauwe <nico@bluepundit.eu>
2024-11-25 22:56:07 +0100

    REFACTOR :hammer: Move everything to CDN for tailwind (not Vite)

--- resources/views/layouts/site/layout.blade.php
+++ resources/views/layouts/site/layout.blade.php
@@ -13,6 +13,8 @@
 
     <!-- Styles / Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
 
 </head>
 <body class="font-sans antialiased h-full">
