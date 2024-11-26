be3900adfa4d61ede866ab47010b17af4a32e210
Nico Deblauwe <nico@bluepundit.eu>
2024-11-26 12:28:16 +0100

    FEATURE :sparkles: Cache the frontpage articles (and relations) loaded

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,8 +8,15 @@ class WelcomeController extends Controller
 {
     public function __invoke()
     {
-        $articles = Article::published()->take(4)->with('author', 'categories', 'comments')->get()->sortByDesc('published_at');
 
+
+        $articles = cache()->remember('welcome_articles',3600, function() {
+            return Article::published()->take(4)
+                ->with('author', 'categories', 'comments')
+                ->get()->sortByDesc('published_at');
+        } );
+
+        return view('welcome')->with('articles', $articles);
         //return view('welcome', compact('articles'));
     }
 }
