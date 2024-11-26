ab1abfe0fae3a1bbc4ba7d46e04f1a63a94a6f6a
Nico Deblauwe <nico@bluepundit.eu>
2024-11-26 12:21:06 +0100

    FEATURE :sparkles: Switch to CDN for assets (instead of vite)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
     </head>
