22d70bd0ef6f0fecc7a62635c6add0f20da10b26
Nico Deblauwe <nico@bluepundit.eu>
2024-11-28 11:42:56 +0100

    REFACTOR :hammer: Article fetching cleaner way

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,11 +17,8 @@ class ArticleController extends Controller
 
     public function show(int $id)
     {
-        $article = Article::published()->where('id', $id)->first();
-
-        if ($article === null) {
-            abort(404);
-        }
+        $article = Article::published()->findOrFail($id);
+        
 
         return view('articles.show')->with('article', $article);
     }
