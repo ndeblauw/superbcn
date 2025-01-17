f8731389f3edf69c633884a868181d4ec67382ce
Nico Deblauwe <nico@bluepundit.eu>
2025-01-17 09:15:59 +0100

    BUGFIX :bug: Remove typo from backup cleaning script runner

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('php artisan backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
+Schedule::command('backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
 Schedule::command('backup:run --only-db')->dailyAt('03:15')->timezone('Europe/Madrid');
 Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
 
