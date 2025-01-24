28b027d0f43623c9c48e1a523885f723dd61bc49
Nico Deblauwe <nico@bluepundit.eu>
2025-01-24 09:48:14 +0100

    FEATURE :sparkles: Show article image in public area of site

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,28 +1,42 @@
 <x-site-layout title="Articles list">
 
     @foreach($articles as $article)
-        <a href="{{route('articles.show', $article->id)}}" class="mt-4">
-            <h2 class="font-bold text-lg">{{$article->title}}</h2>
-            <div>
-                {{ $article->published_at->format('Y-M-d') }}
-                |
-                {{$article->author?->name ?? 'Unknown'}}
+        <div class="flex gap-x-4">
+            <div class="w-20">
+                @if($article->media->first() !== null)
+                    <img src="{{$article->media->first()->getUrl('preview')}}">
+                @else
+                    <img src="{{asset('images/default.webp')}}"/>
+                @endif
             </div>
             <div>
-                @foreach($article->categories as $category)
-                    <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs">{{$category->title}}</span>
-                @endforeach
+                <a href="{{route('articles.show', $article->id)}}" class="mt-4">
+                    <h2 class="font-bold text-lg">{{$article->title}}</h2>
+                    <div>
+                        {{ $article->published_at->format('Y-M-d') }}
+                        |
+                        {{$article->author?->name ?? 'Unknown'}}
+                    </div>
+                    <div>
+                        @foreach($article->categories as $category)
+                            <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs">{{$category->title}}</span>
+                        @endforeach
+                    </div>
+
+
+                    <p class="text-sm">{{ $article->summary(250) }}</p>
+
+                    <ul class="list-disc pl-4">
+                        @foreach($article->comments->take(3) as $comment)
+                            <li>{{$comment->content}}</li>
+                        @endforeach
+                    </ul>
+                </a>
+
             </div>
 
+        </div>
 
-            <p class="text-sm">{{ $article->summary(250) }}</p>
-
-            <ul class="list-disc pl-4">
-                @foreach($article->comments->take(3) as $comment)
-                    <li>{{$comment->content}}</li>
-                @endforeach
-            </ul>
-        </a>
     @endforeach
 
 </x-site-layout>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -11,13 +11,22 @@
             @auth
                 <a class="bg-red-200 p-2 rounded text-red-800" href="{{route('articles.buy', ['slug' => $article->slug, 'amount' => 5])}}">BUY NOW FOR 5 EUR</a>
                 <a class="bg-red-200 p-2 rounded text-red-800" href="{{route('articles.buy', ['slug' => $article->slug, 'amount' => 10])}}">BUY NOW FOR 10 EUR</a>
-
             @endauth
             @guest
                 <a class="bg-red-200 p-2 rounded text-red-800" href="{{route('login')}}">LOG IN to be able to buy</a>
 
             @endguest
         </div>
+
+            <div>by {{$article->author->name}}</div>
+
+            <div>@foreach($article->categories as $category) {{$category->title}} @endforeach</div>
+
+            @if($article->media->first() !== null)
+                <img src="{{$article->media->first()->getUrl('preview')}}">
+            @else
+                <img src="{{asset('images/default.webp')}}"/>
+            @endif
 
         <p class="text-xl">
             {!! $article->getContent() !!}
